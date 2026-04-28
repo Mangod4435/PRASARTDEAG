cfb44b03dd95934415de676df9c1686863861bea
Mangod4435 <nathanonk29@gmail.com>
2026-04-28 21:07:34 +0700

    Initial Commit

---
+++ PRASARTDAEG/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PRASARTDAEG
+//
+//  Created by Nathanon K on 28/4/2569 BE.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+}
---
+++ PRASARTDAEG/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  PRASARTDAEG
+//
+//  Created by Nathanon K on 28/4/2569 BE.
+//
+
+import Cocoa
+import GameplayKit
+import SpriteKit
+
+class ViewController: NSViewController {
+
+	@IBOutlet var skView: SKView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+		// including entities and graphs.
+		if let scene = GKScene(fileNamed: "GameScene") {
+
+			// Get the SKScene from the loaded GKScene
+			if let sceneNode = scene.rootNode as! GameScene? {
+
+				// Copy gameplay related content over to the scene
+				sceneNode.entities = scene.entities
+				sceneNode.graphs = scene.graphs
+
+				// Set the scale mode to scale to fit the window
+				sceneNode.scaleMode = .aspectFill
+
+				// Present the scene
+				if let view = self.skView {
+					view.presentScene(sceneNode)
+
+					view.ignoresSiblingOrder = true
+
+					view.showsFPS = true
+					view.showsNodeCount = true
+				}
+			}
+		}
+	}
+}
